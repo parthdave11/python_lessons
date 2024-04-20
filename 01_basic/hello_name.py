f4d9f21f1c10930d9f9976fbd0ad6a43dcb5e925
Parth Dave <daveparth422@gmail.com>
2024-04-20 17:18:33 +0530

    python_lessons conditionals

---
+++ 01_basic/hello_name.py
@@ -0,0 +1,13 @@
+print("Parth Dave")
+
+def name(n):
+    print(n)
+
+name("Conor Macgregor")
+
+name_one = "Anderson Silva"
+name_two = "Jon Jones"
+name_three = "Dimitrious Jhonson"
+
+# read about reor, str and print.
+# decimal, fraction library(decimal context manager)
---
+++ 01_basic/name.py
@@ -0,0 +1,3 @@
+from hello_name import name
+
+name("Floyd Maywather")
